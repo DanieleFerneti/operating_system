448f825c196bd950f0a6f1aa54a0ec949f131693
DanieleFerneti <108084627+DanieleFerneti@users.noreply.github.com>
2023-11-18 19:00:39 +0100

    Update secondoEsercizio.c

--- primo_homework/secondoEsercizio.c
+++ primo_homework/secondoEsercizio.c
@@ -7,19 +7,19 @@
 
 
 int main(int argc , char *argv[]){
-		int file = open("/home/file.txt",O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
-		printf("%d \n",file);
-		int rc = fork();
-		if(rc < 0){
-			fprintf(stderr, "fork failed\n");
-			exit(1);
-		} else if (rc == 0){
-			printf("child %d e il mio pid è: %d \n",file,(int) getpid());
+	int file = open("/home/file.txt",O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+	printf("%d \n",file);
+	int rc = fork();
+	if(rc < 0){
+		fprintf(stderr, "fork failed\n");
+		exit(1);
+	} else if (rc == 0){
+		printf("child %d e il mio pid è: %d \n",file,(int) getpid());
 
-		} else {
-			printf("parent %d e il mio pid è %d \n",file,(int) getpid());
+	} else {
+		printf("parent %d e il mio pid è %d \n",file,(int) getpid());
 
-		}
-		return 0;
+	}
+	return 0;
 
 }
