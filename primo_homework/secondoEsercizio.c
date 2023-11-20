a180ef8e819db0f39bb28d209d2d8bcfb05aa92e
DanieleFerneti <108084627+DanieleFerneti@users.noreply.github.com>
2023-11-20 22:10:52 +0100

    Update secondoEsercizio.c

--- primo_homework/secondoEsercizio.c
+++ primo_homework/secondoEsercizio.c
@@ -7,19 +7,25 @@
 
 
 int main(int argc , char *argv[]){
-	int file = open("./file.output",O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
-	printf("%d \n",file);
-	int rc = fork();
-	if(rc < 0){
-		fprintf(stderr, "fork failed\n");
-		exit(1);
-	} else if (rc == 0){
-		printf("child %d e il mio pid è: %d \n",file,(int) getpid());
+		
+		int file = open("./file.output",O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+		int rc = fork();
+		if(rc < 0){
+			fprintf(stderr, "fork failed\n");
+			exit(1);
+		} else if (rc == 0){
 
-	} else {
-		printf("parent %d e il mio pid è %d \n",file,(int) getpid());
+			printf("child %d e il mio pid è: %d \n",file,(int) getpid());
+			
+			write(file,"hello\n",5);
+			close(file);
 
-	}
-	return 0;
+		} else {
+			printf("parent %d e il mio pid è %d \n",file,(int) getpid());
+			write(file,"hey\n",9);
+			close(file);				
+
+		}
+		return 0;
 
 }
